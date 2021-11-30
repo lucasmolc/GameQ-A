26f8a96681eba40f700822760b11232d77b2a45d
Daniel Costa <dfscs.costa@gmail.com>
2021-11-29 23:23:45 -0300

    Interface principal.

--- jogoDoVintao/src/mainForm.java
+++ jogoDoVintao/src/mainForm.java
@@ -10,19 +10,19 @@ public class mainForm extends javax.swing.JFrame {
    
     public static final String VERSAONOVA = "1";
     public static final String VERSAO = "v" + VERSAONOVA + " 26/11/2021";
+    public boolean player = false;
+    public String selectedPlayer = "a";
     public static  Container getContantPane() 
     {
         throw new UnsupportedOperationException("Not supported yet."); 
     }
     
     public mainForm() {
-        setUndecorated(true);
         initComponents();
         setTitle("Jogo do Vintão - " + VERSAO);
         Container fContainer = getContentPane();
         fContainer.setBackground(Color.DARK_GRAY);
         fContainer.setForeground(Color.WHITE);
-        setOpacity(0.99f);
         setLocationRelativeTo(null);
     }
 
@@ -31,13 +31,20 @@ public class mainForm extends javax.swing.JFrame {
 
         telaLateral = new javax.swing.JPanel();
         jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jButton10 = new javax.swing.JButton();
         telaPrincipal = new javax.swing.JPanel();
         playButton = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        Personagem = new javax.swing.JLabel();
+        imgPlayer2 = new javax.swing.JLabel();
+        imgPlayer1 = new javax.swing.JLabel();
+        lblPlayer2 = new javax.swing.JLabel();
+        lblPlayer1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(226, 222, 226));
@@ -46,44 +53,45 @@ public class mainForm extends javax.swing.JFrame {
 
         telaLateral.setBackground(new java.awt.Color(241, 246, 248));
         telaLateral.setForeground(new java.awt.Color(241, 246, 248));
+        telaLateral.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/botõesLateraldegrade.png"))); // NOI18N
         jButton3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jButton3.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         jButton3.setDefaultCapable(false);
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        telaLateral.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 290, -1, 74));
 
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/instrucoes.png"))); // NOI18N
-        jButton4.setText("Instruções");
-        jButton4.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jLabel2.setForeground(new java.awt.Color(15, 15, 15));
+        jLabel2.setText("SELECIONE JOGADOR 1");
+        telaLateral.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 40, 160, 40));
 
-        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/historico.png"))); // NOI18N
-        jButton5.setText("Histórico");
-        jButton5.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        telaLateral.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 110, -1, -1));
 
-        javax.swing.GroupLayout telaLateralLayout = new javax.swing.GroupLayout(telaLateral);
-        telaLateral.setLayout(telaLateralLayout);
-        telaLateralLayout.setHorizontalGroup(
-            telaLateralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(telaLateralLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(telaLateralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton3)
-                    .addGroup(telaLateralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(jButton4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                        .addComponent(jButton5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(20, 20, 20))
-        );
-        telaLateralLayout.setVerticalGroup(
-            telaLateralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(telaLateralLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(45, 45, 45)
-                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(43, 43, 43)
-                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(36, 36, 36))
-        );
+        jButton6.setText("jButton6");
+        telaLateral.add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 110, -1, -1));
+
+        jButton7.setText("jButton7");
+        telaLateral.add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, -1, -1));
+
+        jButton8.setText("jButton8");
+        telaLateral.add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 160, -1, -1));
+
+        jButton9.setText("jButton9");
+        telaLateral.add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 210, -1, -1));
+
+        jButton10.setText("jButton10");
+        telaLateral.add(jButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 210, 80, -1));
 
         telaPrincipal.setBackground(new java.awt.Color(89, 134, 173));
         telaPrincipal.setForeground(new java.awt.Color(89, 134, 173));
@@ -103,42 +111,47 @@ public class mainForm extends javax.swing.JFrame {
                 playButtonActionPerformed(evt);
             }
         });
-        telaPrincipal.add(playButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 270, 180, 80));
-
-        jButton2.setBackground(new java.awt.Color(211, 211, 211));
-        jButton2.setForeground(new java.awt.Color(29, 29, 29));
-        jButton2.setFocusPainted(false);
-        jButton2.setLabel("X");
-        jButton2.setName("btnClose"); // NOI18N
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-        telaPrincipal.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 10, 50, 30));
+        telaPrincipal.add(playButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 280, 180, 80));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(235, 235, 235));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/title.png"))); // NOI18N
-        telaPrincipal.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, 330, 90));
+        telaPrincipal.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, 340, 90));
 
-        Personagem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/anonimoreformed.png"))); // NOI18N
-        telaPrincipal.add(Personagem, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 120, 210, 280));
+        imgPlayer2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/anonimoreformed.png"))); // NOI18N
+        imgPlayer2.setEnabled(false);
+        telaPrincipal.add(imgPlayer2, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 150, 170, 220));
+
+        imgPlayer1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/anonimoreformed.png"))); // NOI18N
+        imgPlayer1.setEnabled(false);
+        telaPrincipal.add(imgPlayer1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 150, -1, 220));
+
+        lblPlayer2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblPlayer2.setText("PLAYER 2");
+        lblPlayer2.setEnabled(false);
+        telaPrincipal.add(lblPlayer2, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 370, -1, -1));
+
+        lblPlayer1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblPlayer1.setText("PLAYER 1");
+        lblPlayer1.setEnabled(false);
+        telaPrincipal.add(lblPlayer1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 370, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(telaLateral, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(telaLateral, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(telaPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 547, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(telaLateral, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(telaPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 418, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(telaPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 410, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -151,9 +164,16 @@ public class mainForm extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_playButtonActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        selectPlayer(player);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        if(selectedPlayer != null && selectedPlayer != ""){
+            player = true;
+            
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     public static void main(String args[]) {
         try{
@@ -176,14 +196,30 @@ public class mainForm extends javax.swing.JFrame {
             }
         });
     }
+    public void selectPlayer(boolean player){
+        if(player == false ){
+           imgPlayer1.setEnabled(true);
+           lblPlayer1.setEnabled(true);
+        } else{
+           imgPlayer2.setEnabled(true);
+           lblPlayer2.setEnabled(true);
+        }
+    }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Personagem;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel imgPlayer1;
+    private javax.swing.JLabel imgPlayer2;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel lblPlayer1;
+    private javax.swing.JLabel lblPlayer2;
     private javax.swing.JButton playButton;
     private javax.swing.JPanel telaLateral;
     private javax.swing.JPanel telaPrincipal;
